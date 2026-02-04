81b42e413982c89c69c14859526f7ab8a072d2ae
marahnofal <marahmahmoud4@gmail.com>
2026-02-04 15:27:33 +0200

    finall

--- src/Pages/Otp/Otp.jsx
+++ src/Pages/Otp/Otp.jsx
@@ -5,7 +5,7 @@ export default function Otp() {
   return (
      <>
      <div className='flex flex-col items-center justify-center w-full h-screen '>
-           <div  className='flex flex-col   w-[445px] h-[440px] px-5 gap-5'>
+           <div  className='flex flex-col w-full  md:w-1/3 md:h-1/3 px-5 gap-5'>
             
             <Link to={'/login/forgetpassword'} className='font-bold'><i className='fa-solid fa-angle-left'></i>Back </Link>
             
@@ -16,37 +16,37 @@ export default function Otp() {
              <form action="" className='flex flex-wrap  gap-5 '>
                 
                  <input type="text" maxLength="1"
-                    className="w-12 h-16 text-center text-2xl border-2 border-blue-500 rounded-xl
+                    className="md:w-12 md:h-16 w-8 h-10 text-center text-2xl border-2 border-blue-500 rounded-xl
                     focus:outline-none focus:ring-2 focus:ring-blue-500
                      
                     transition-transform duration-300 hover:scale-110"></input>
                 
                  <input type="text" maxLength="1"
-                    className="w-12 h-16 text-center text-2xl border-2 border-blue-500 rounded-xl
+                    className="md:w-12 md:h-16 w-8 h-10 text-center text-2xl border-2 border-blue-500 rounded-xl
                     focus:outline-none focus:ring-2 focus:ring-blue-500
                      
                     transition-transform duration-300 hover:scale-110"></input>
                 
                  <input type="text" maxLength="1"
-                    className="w-12 h-16 text-center text-2xl border-2 border-blue-500 rounded-xl
+                    className="md:w-12 md:h-16 w-8 h-10 text-center text-2xl border-2 border-blue-500 rounded-xl
                     focus:outline-none focus:ring-2 focus:ring-blue-500
                      
                     transition-transform duration-300 hover:scale-110"></input>
                 
                  <input type="text" maxLength="1"
-                    className="w-12 h-16 text-center text-2xl border-2 border-blue-500 rounded-xl
+                    className="md:w-12 md:h-16 w-8 h-10 text-center text-2xl border-2 border-blue-500 rounded-xl
                     focus:outline-none focus:ring-2 focus:ring-blue-500
                      
                     transition-transform duration-300 hover:scale-110"></input>
                 
                  <input type="text" maxLength="1"
-                    className="w-12 h-16 text-center text-2xl border-2 border-blue-500 rounded-xl
+                    className="md:w-12 md:h-16 w-8 h-10 text-center text-2xl border-2 border-blue-500 rounded-xl
                     focus:outline-none focus:ring-2 focus:ring-blue-500
                      
                     transition-transform duration-300 hover:scale-110"></input>
                 
                  <input type="text" maxLength="1"
-                    className="w-12 h-16 text-center text-2xl border-2 border-blue-500 rounded-xl
+                    className="md:w-12 md:h-16 w-8 h-10 text-center text-2xl border-2 border-blue-500 rounded-xl
                     focus:outline-none focus:ring-2 focus:ring-blue-500
                      
                     transition-transform duration-300 hover:scale-110"></input>
